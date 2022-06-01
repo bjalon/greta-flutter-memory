8befc63c540348f9558735ecba4a20151bfea5b2
Benjamin JALON <bjalon@qastia.com>
2022-06-01 12:28:30 +0200

    Affiche/cache la carte

--- lib/main.dart
+++ lib/main.dart
@@ -40,21 +40,44 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class CardWidget extends StatelessWidget {
+class CardWidget extends StatefulWidget {
   final String value;
 
   const CardWidget(this.value, {Key? key}) : super(key: key);
 
   @override
+  State<CardWidget> createState() => _CardWidgetState();
+}
+
+class _CardWidgetState extends State<CardWidget> {
+  var isShown = false;
+
+  @override
   Widget build(BuildContext context) {
+    // var textAAfficher;
+    // if (isShown) {
+    //   textAAfficher = widget.value;
+    // } else {
+    //   textAAfficher = "?";
+    // }
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
           decoration:
               BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-          child: IconButton(onPressed: clickedCard, icon: Text(value))),
+          child: IconButton(onPressed: clickedCard, icon: Text(isShown ? widget.value : "?"))),
     );
   }
 
-  void clickedCard() {}
+  void clickedCard() {
+    setState(() {
+      isShown = !isShown;
+      //
+      // if (isShown) {
+      //   isShown = false;
+      // } else {
+      //   isShown = true;
+      // }
+    });
+  }
 }
